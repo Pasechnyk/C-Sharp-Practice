e547bbdf0013013cf105b3285bde1917e1369679
Pasechnyk <101930828+Pasechnyk@users.noreply.github.com>
2022-11-12 22:58:54 +0200

    Add files via upload

---
+++ C# HW7 - Inheritance in Instrument Classes.cs
@@ -0,0 +1,115 @@
+﻿using System;
+
+namespace Musical_Instruments
+{
+    // Task - create Instrument hierarchy with inheritance using abstract class
+    public enum Category { Woodwind, Brass, String, Keyboard, Percussion}
+    public abstract class Instrument
+    {
+        // properties
+        public string Name { get; set; }
+        public Category Type { get; set; }
+        public string Model { get; set; }
+
+        // contructors
+        public Instrument() { }
+        public Instrument(string name, Category type, string model)
+        {
+            Name = name;
+            Type = type;
+            Model = model;
+        }
+
+        // abstract methods
+        public abstract void Play();
+        public abstract void SpecialInfo();
+    }
+
+    public class Piano : Instrument
+    {
+        // property
+        public int Octaves { get; set; }
+
+        // contructors
+        public Piano() { }
+        public Piano (string name, Category type, string model, int octave) : base (name, type, model)
+        {
+            Octaves = octave;
+        }
+
+        // override methods
+        public override void Play()
+        {
+            Console.WriteLine("You've used the keyboard on the piano!");
+        }
+        public override void SpecialInfo()
+        {
+            Console.WriteLine($"This instrument has {Octaves} octaves!");
+        }
+    }
+
+    public class Violin : Instrument
+    {
+        // properties
+        public string Materials { get; set; }
+
+        // contructors
+        public Violin() { }
+        public Violin(string name, Category type, string model, string materials) : base(name, type, model)
+        {
+            Materials = materials;
+        }
+
+        // override methods
+        public override void Play()
+        {
+            Console.WriteLine("You've used the strings on the violin!");
+        }
+        public override void SpecialInfo()
+        {
+            Console.WriteLine($"This instrument is made out of {Materials}!");
+        }
+    }
+    public class Trumpet : Instrument
+    {
+        // property
+        public string TrumpetType { get; set; }
+
+        // contructors
+        public Trumpet() { }
+        public Trumpet(string name, Category type, string model, string tType) : base(name, type, model)
+        {
+            TrumpetType = tType;
+        }
+
+        // override methods
+        public override void Play()
+        {
+            Console.WriteLine("You've used the brass on the trumpet!");
+        }
+        public override void SpecialInfo()
+        {
+            Console.WriteLine($"This instrument is of {TrumpetType} type!");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // initialize intruments
+
+            Piano piano = new Piano("Yamaha", Category.Keyboard, "JU7225", 8);
+            piano.Play();
+            piano.SpecialInfo();
+
+            Violin violin = new Violin("Eastar", Category.String, "UO85", "Acacia");
+            violin.Play();
+            violin.SpecialInfo();
+
+            Trumpet trumpet = new Trumpet("Yamaha", Category.Brass, "B Trumpet", "B-flat");
+            trumpet.Play();
+            trumpet.SpecialInfo();
+        }
+    }
+}
---
+++ C# HW8 - Overloaded Operators with Time Class.cs
@@ -0,0 +1,147 @@
+﻿using System;
+
+namespace Time
+{
+    // Task - create Time class using overloaded operators
+    public class Time
+    {
+        // properties
+        public int Hours { get; set; }
+        public int Minutes { get; set; }
+        public int Seconds { get; set; }
+
+        // constructors
+        public Time() { }
+        public Time(int hour, int minute, int second)
+        {
+            Hours = hour;
+            Minutes = minute;
+            Seconds = second;
+        }
+
+        // turn the seconds into hh:mm:ss
+        public Time(int second)
+        {
+            Hours = second / 3600;
+            Minutes = (second - Hours * 3600) / 60;
+            Seconds = second - Hours * 3600 - Minutes * 60;
+        }
+
+        // operators
+        public static Time operator ++(Time t)
+        {
+            if (t.Seconds < 59)
+            {
+                t.Seconds += 1;
+            }
+            else { t.Seconds = 00; t.Minutes += 1; }
+            return t;
+        }
+        public static Time operator --(Time t)
+        {
+            if (t.Seconds > 00)
+            {
+                t.Seconds -= 1;
+            }
+            else { t.Seconds = 59; t.Minutes -= 1; }
+            return t;
+        }
+
+        // > >= <= <
+        public static bool operator >(Time left, Time right)
+        {
+            return left.Hours > right.Hours;
+        }
+        public static bool operator <(Time left, Time right)
+        {
+            return !(left.Hours > right.Hours);
+        }
+        public static bool operator >=(Time left, Time right)
+        {
+            return left.Hours >= right.Hours;
+        }
+        public static bool operator <=(Time left, Time right)
+        {
+            return !(left.Hours >= right.Hours);
+        }
+
+        // equals operator with hash code
+        public override bool Equals(object obj)
+        {
+            return obj is Time t && Hours == t?.Hours;
+        }
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Hours);
+        }
+        public static bool operator ==(Time left, Time right)
+        {
+            return left.Equals(right);
+        }
+        public static bool operator !=(Time left, Time right)
+        {
+            return !(left == right);
+        }
+
+        // ariphmetic operators + and -
+        public static Time operator +(Time left, Time right)
+        {
+            Time t = new(left.Hours, right.Minutes)
+            {
+                Seconds = left.Seconds + right.Seconds
+            };
+            return t;
+        }
+        public static Time operator -(Time left, Time right)
+        {
+            Time t = new(left.Hours, right.Minutes)
+            {
+                Seconds = left.Seconds - right.Seconds
+            };
+            return t;
+        }
+
+        // methods
+        public void ShowTime()
+        {
+            Console.WriteLine($"Your given time is {Hours}:{Minutes}:{Seconds}");
+        }
+        public void Reset()
+        {
+            Hours = 0;
+            Minutes = 0;
+            Seconds = 0;
+
+            Console.WriteLine("Time has benn reset!");
+            Console.WriteLine($"Reset time - {Hours}:{Minutes}:{Seconds}");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Time t1 = new Time(14, 26, 02);
+            ++t1;
+            t1.ShowTime();
+
+            Time t2 = new Time(2966);
+            t2.ShowTime();
+
+            Time t3 = t1 + t2;
+            t3.ShowTime();
+
+            if (t1 > t2)
+            {
+                Console.WriteLine("Time 1 has bigger hour value than Time 2!");
+            }
+
+            if (t1 == t2)
+            {
+                Console.WriteLine("Time 1 and Time 2 are equal!");
+            }
+
+            t2.Reset();
+        }
+    }
+}
