53c36bd1ec39c6455faf478fe44434d01904a78a
Pasechnyk <101930828+Pasechnyk@users.noreply.github.com>
2022-11-23 22:04:17 +0200

    Add files via upload

---
+++ C# HW10 - Dictionary collection with App.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+
+// Task - create managment system for users using Dictionary collection
+namespace Manager
+{
+    public class ManagerApp
+    {
+        // dictionary collection
+        private Dictionary<string, string> UserInformation { get; set; }
+
+        // methods
+        public void AddUser(string login, string password)
+        {
+            UserInformation.Add(login, password);
+        }
+
+        public void RemoveLogin(string login)
+        {
+            if (UserInformation.ContainsKey(login))
+            {
+                UserInformation.Remove(login);
+            }
+            else { Console.WriteLine("Cannot hold the operation due to the missing login!"); }
+        }
+
+        public void EditUser(string login, string password)
+        {
+            UserInformation[login] = password;
+        }
+
+        public void FindPassword(string login)
+        {
+            if (UserInformation.ContainsKey(login))
+            {
+                Console.WriteLine(UserInformation[login]);
+            }
+            else { Console.WriteLine("There is no such login in a system!"); }
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // initializing app class
+            ManagerApp manager = new ManagerApp();
+
+            Dictionary<string, string> UserInformation = new Dictionary<string, string>
+            {
+                ["Paul"] = "Yjco25",
+                ["isak"] = "kainfo8ej",
+                ["juen"] = "Imrof778899",
+                ["Vivi"] = "JneiOmel",
+                ["Pola"] = "Ioooppp54",
+                ["Tony"] = "Win733",
+            };
+
+            // menu with switch
+            Console.WriteLine("------------ USER MANAGEMENT APP ------------");
+            Console.WriteLine("Choose your option: \n 1 - Show collection of users \n 2 - Add new user" +
+                "\n 3 - Remove existing login \n 4 - Edit user information \n 5 - Find password by given login");
+            int option;
+            option = Console.Read();
+            switch (option)
+            {
+                case 1:
+                    {
+                        Console.WriteLine("Collection of users:");
+                        foreach (var user in UserInformation)
+                        {
+                            Console.WriteLine($"{user.Key} - {user.Value}");
+                        }
+                    }
+                    break;
+                case 2:
+                    {
+                        Console.WriteLine("Enter the login and password you want to ADD:");
+                        string login = Console.ReadLine();
+                        string password = Console.ReadLine();
+                        manager.AddUser(login, password);
+                    }
+                    break;
+                case 3:
+                    {
+                        Console.WriteLine("Enter the login you want to REMOVE:");
+                        string login = Console.ReadLine();
+                        manager.RemoveLogin(login);
+                    }
+                    break;
+                case 4:
+                    {
+                        Console.WriteLine("Enter the login and password you want to EDIT:");
+                        string login = Console.ReadLine();
+                        string password = Console.ReadLine();
+                        manager.EditUser(login, password);
+                    }
+                    break;
+                case 5:
+                    {
+                        Console.WriteLine("Enter the login to FIND password information:");
+                        string login = Console.ReadLine();
+                        manager.FindPassword(login);
+                    }
+                    break;
+                default:
+                    break;
+            }    
+        }
+    }
+}
---
+++ C# HW9 - Delegates with Arrays.cs
@@ -0,0 +1,82 @@
+﻿using System;
+
+namespace Delegates
+{
+    // Task - create several arrays with different conditions using delegate system
+    delegate int ArrayInitializer(int value);
+
+    class Program
+    {
+        // delegate methods
+        public static void Initialize (int[] arr, Func<int, int> init)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = init(i);
+            }
+        }
+
+        public static void ModifyArray(int[] arr, ArrayInitializer init)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = init(arr[i]);
+            }
+        }
+
+        // method with Fibonacci numbers
+        public static int Fibonacci(int value)
+        {
+            int firstNumber = 0, secondNumber = 1, result = 0;
+            if (value == 0)
+                return 0;
+            if (value == 1)
+                return 1;
+            for (int i = 2; i <= value; i++)
+            {
+                result = firstNumber + secondNumber;
+                firstNumber = secondNumber;
+                secondNumber = result;
+            }
+            return result;
+
+        }
+
+        // show array
+        public static void ShowArray(int[] arr, string text)
+        {
+            Console.Write($"{text}: ");
+            foreach (var i in arr)
+            {
+                Console.Write(i + " ");
+                Console.WriteLine();
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            // anonymous delegate methods
+            // random values
+            int[] num1 = new int[15];
+            Initialize(num1, (i) => new Random().Next(100));
+            ShowArray(num1, "Array with RANDOM values:");
+
+            // values of 2 decree
+            int[] num2 = new int[10];
+            Initialize(num2, (i) => (int)Math.Pow(2, i));
+            ShowArray(num2, "Array with 2 in DECREE:");
+
+            // values of numbers multiplied by 3
+            int[] num3 = new int[20];
+            Initialize(num3, (i) => i * 3);
+            ShowArray(num3, "Array with numbers MULTIPLIED by 3:");
+
+            // using delegate as method parameter
+            // Fibonacci numbers
+            int[] num4 = new int[20];
+            ModifyArray(num4, Fibonacci);
+            ShowArray(num4, "Array with FIBONACCI numbers:");
+
+        }
+    }
+}
